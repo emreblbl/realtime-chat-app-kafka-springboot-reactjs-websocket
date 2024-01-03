4d34123e94d379c39a67931eb9225212cb0f2f3d
Emre Bülbül <61878543+emreblbl@users.noreply.github.com>
2024-01-03 21:18:30 +0300

    WebSocketConfig has been refactored.

--- kafka-chat-server/src/main/java/io/github/emreblbl/kafkachatserver/service/MessageListener.java
+++ kafka-chat-server/src/main/java/io/github/emreblbl/kafkachatserver/service/MessageListener.java
@@ -2,27 +2,22 @@ package io.github.emreblbl.kafkachatserver.service;
 
 import io.github.emreblbl.kafkachatserver.constant.KafkaConstants;
 import io.github.emreblbl.kafkachatserver.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageListener {
+    @Autowired
+    SimpMessagingTemplate template;
 
-
-    final SimpMessagingTemplate template;
-
-
-    public MessageListener(SimpMessagingTemplate template) {
-        this.template = template;
-    }
     @KafkaListener(
             topics = KafkaConstants.KAFKA_TOPIC,
             groupId = KafkaConstants.GROUP_ID
     )
-
-    public void listen(Message message){
+    public void listen(Message message) {
         System.out.println("sending via kafka listener..");
-        template.convertAndSend("/topic/group",message);
+        template.convertAndSend("/topic/group", message);
     }
 }
